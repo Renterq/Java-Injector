a8b5bd50627ad9d969ead2d22e0ca46e1020017f
Renter <erenerte0@gmail.com>
2026-05-15 15:56:36 +0300

    Remove comment regarding custom library definition
    Removed comment about defining a custom library to avoid method conflicts.

--- Injector.java
+++ Injector.java
@@ -10,7 +10,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 public class Injector {
-    // Kernel32'yi miras almadan kendi kütüphanemizi tanımlıyoruz ki IDEA method çakışması (Ambiguous) hatası vermesin.
     public interface MyKernel32 extends Library {
         MyKernel32 INSTANCE = Native.load("kernel32", MyKernel32.class);
         
